4575d4ea4b512128bc72056709cfaa3b667004a3
AN-Fahmy <fahmyan@taibaconnect.com>
2025-08-08 21:47:58 +0300

    title

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { BlankComponent } from './layout/blank/blank.component';
 import { ContactComponent } from './layout/contact/contact.component';
 
 export const routes: Routes = [
-  {path:'', redirectTo:'taiba-connect', pathMatch:'full'},
-  {path:'taiba-connect', component:BlankComponent, title:'taiba connect'},
-  {path:'contact', component:ContactComponent, title:'Contact Us'},
+  // {path:'', redirectTo:'taiba-connect', pathMatch:'full'},
+  {path:'', component:BlankComponent, title:'Tech2Earn'},
+  // {path:'contact', component:ContactComponent, title:'Contact Us'},
 ];
