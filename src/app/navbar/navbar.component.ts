d8929c81b6cebcb26879ff1e0978b9431ced01aa
AN-Fahmy <fahmyan@taibaconnect.com>
2025-08-10 10:04:31 +0300

    home and navbar design

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -10,26 +10,17 @@ import { Component, HostListener, inject, PLATFORM_ID } from '@angular/core';
 })
 export class NavbarComponent{
   private readonly _PLATFORM_ID = inject(PLATFORM_ID)
-  navbarWidth:string = '75%'
-  navbarLeft:string = '12.5%'
-  navbarTop:string = '15px'
-  background:string = '#ffffff4d'
+  navbarTop:string = '0'
+  background:string = 'transparent'
   lastScrollTop = 0;
-  isNavbarVisible = true;
 
   @HostListener('window:scroll') onScroll(){
     const scrollPosition = window.scrollY;
 
     if (scrollPosition > 200) {
-      this.navbarWidth = '100%';
-      this.navbarLeft = '0';
-      this.navbarTop = '0';
-      this.background = '#019fe35f'
+      this.background = '#1286957a'
     } else {
-      this.navbarWidth = '75%';
-      this.navbarLeft = '12.5%';
-      this.navbarTop = '15px';
-      this.background = '#ffffff4d'
+      this.background = 'transparent'
     }
 
     if (scrollPosition > this.lastScrollTop) {
@@ -39,8 +30,6 @@ export class NavbarComponent{
     } else {
       if(this.lastScrollTop > 200){
         this.navbarTop = '0';
-      } else {
-        this.navbarTop = '15px';
       }
     }
 
@@ -52,7 +41,6 @@ export class NavbarComponent{
     { id: 'home', name: 'Home' },
     { id: 'about', name: 'About' },
     { id: 'feature', name: 'Features' },
-    { id: 'ceo', name: 'Ceo' },
     { id: 'why-us', name: 'Why Choose Us' },
     { id: 'our-work', name: 'Our Work' },
     { id: 'partners', name: 'Our Partners' },
